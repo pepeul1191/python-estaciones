c6f661b68b69d9f10eacd35923d96bbd0267849c
Pepe Valdivia <jvaldivia@softweb.pe>
2017-12-04 15:06:01 -0500

    CRUD de unidades de medida

--- test/rspec/unidad_medida.rb
+++ test/rspec/unidad_medida.rb
@@ -21,4 +21,136 @@ def listar
   end
 end
 
+def crear
+  RSpec.describe App do
+    describe "2. Crear unidades de medida: " do
+      it '2.1 Conexión con backend' do
+        url = 'test/conexion'
+        test = App.new(url)
+        test.get()
+        expect(test.response.code).to eq(200)
+      end
+      it '2.2 Crear unidades de medida' do
+        data = {
+          :nuevos => [
+            {
+              :id => 'tablaAsociacion_482',
+              :nombre => 'Corbett', 
+              :simbolo => 'Dudmarsh', 
+            },
+            {
+              :id => 'tablaAsociacion_483',
+              :nombre => 'Channa', 
+              :simbolo => 'Labarre', 
+            },
+            {
+              :id => 'tablaAsociacion_484',
+              :nombre => 'Maxy', 
+              :simbolo => 'McGlone', 
+            },
+            {
+              :id => 'tablaAsociacion_485',
+              :nombre => 'Alphard', 
+              :simbolo => 'Biddlecombe', 
+            }  
+          ],
+          :editados => [],  
+          :eliminados => []
+        }.to_json
+        url = 'unidad_medida/guardar?data=' + data
+        test = App.new(url)
+        test.post()
+        expect(test.response.code).to eq(200)
+        expect(test.response.body).not_to include('error')
+        expect(test.response.body).to include('Se ha registrado los cambios en los unidades de medida')
+        expect(test.response.body).to include('nuevo_id')
+        expect(test.response.body).to include('success')
+      end
+    end
+  end
+end
+
+def editar
+  RSpec.describe App do
+    describe "3. Editar unidades de medida: " do
+      it '3.1 Conexión con backend' do
+        url = 'test/conexion'
+        test = App.new(url)
+        test.get()
+        expect(test.response.code).to eq(200)
+      end
+      it '3.2 Editar unidades de medida' do
+        data = {
+          :nuevos => [
+          ],
+          :editados => [
+            {
+              :id => '3',
+              :nombre => 'XD', 
+              :simbolo => 'Dudmarsh', 
+            },
+            {
+              :id => '4',
+              :nombre => 'XD', 
+              :simbolo => 'Labarre', 
+            },
+            {
+              :id => '5',
+              :nombre => 'XD', 
+              :simbolo => 'McGlone', 
+            },
+            {
+              :id => '6',
+              :nombre => 'XD', 
+              :simbolo => 'Biddlecombe', 
+            }
+          ],  
+          :eliminados => []
+        }.to_json
+        url = 'unidad_medida/guardar?data=' + data
+        test = App.new(url)
+        test.post()
+        expect(test.response.code).to eq(200)
+        expect(test.response.body).not_to include('error')
+        expect(test.response.body).to include('Se ha registrado los cambios en los unidades de medida')
+        expect(test.response.body).to include('success')
+      end
+    end
+  end
+end
+
+def eliminar
+  RSpec.describe App do
+    describe "4. Eliminar unidades de medida: " do
+      it '4.1 Conexión con backend' do
+        url = 'test/conexion'
+        test = App.new(url)
+        test.get()
+        expect(test.response.code).to eq(200)
+      end
+      it '4.2 Eliminar unidades de medida' do
+        data = {
+          :nuevos => [
+          ],
+          :editados => [
+          ],  
+          :eliminados => [
+            3,4,5,6
+          ]
+        }.to_json
+        url = 'unidad_medida/guardar?data=' + data
+        test = App.new(url)
+        test.post()
+        expect(test.response.code).to eq(200)
+        expect(test.response.body).not_to include('error')
+        expect(test.response.body).to include('Se ha registrado los cambios en los unidades de medida')
+        expect(test.response.body).to include('success')
+      end
+    end
+  end
+end
+
 listar
+#crear
+#editar
+eliminar
--- views/unidad_medida.py
+++ views/unidad_medida.py
@@ -13,3 +13,36 @@ def extension_listar():
 	conn = engine.connect()
 	stmt = select([UnidadMedida])
 	return json.dumps([dict(r) for r in conn.execute(stmt)])
+
+@unidad_medida_view.route('/guardar', method='POST')
+def guardar():
+	data = json.loads(request.query.data)
+	nuevos = data['nuevos']
+	editados = data['editados']
+	eliminados = data['eliminados']
+	array_nuevos = []
+	rpta = None
+	session = session_db()
+	try:
+		if len(nuevos) != 0:
+			for nuevo in nuevos:
+				temp_id = nuevo['id']
+				nombre = nuevo['nombre']
+				simbolo = nuevo['simbolo']
+				s = UnidadMedida(nombre = nombre, simbolo = simbolo)
+				session.add(s)
+				session.flush()
+				temp = {'temporal' : temp_id, 'nuevo_id' : s.id}
+				array_nuevos.append(temp)
+		if len(editados) != 0:
+			for editado in editados:
+				session.query(UnidadMedida).filter_by(id = editado['id']).update(editado)
+		if len(eliminados) != 0:
+			for id in eliminados:
+				session.query(UnidadMedida).filter_by(id = id).delete()
+		session.commit()
+		rpta = {'tipo_mensaje' : 'success', 'mensaje' : ['Se ha registrado los cambios en los unidades de medida', array_nuevos]}
+	except Exception as e:
+		session.rollback()
+		rpta = {'tipo_mensaje' : 'error', 'mensaje' : ['Se ha producido un error en guardar la tabla de unidades de medida', str(e)]}
+	return json.dumps(rpta)
