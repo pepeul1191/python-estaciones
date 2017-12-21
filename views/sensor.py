41fb4e29a37eaee935ad2eef41b1606d2ae9cc50
Pepe Valdivia <jvaldivia@softweb.pe>
2017-12-21 14:53:46 -0500

    Listado de sensores de estacion

--- views/sensor.py
+++ views/sensor.py
@@ -13,3 +13,13 @@ def listar():
 	conn = engine.connect()
 	stmt = select([Sensor])
 	return json.dumps([dict(r) for r in conn.execute(stmt)])
+
+@sensor_view.route('/listar/<estacion_id>', method='GET')
+def listar_estacion(estacion_id):
+	conn = engine.connect()
+	stmt = """
+		SELECT S.id, S.nombre, S.descripcion, U.simbolo 
+		FROM sensores S INNER JOIN unidad_medidas U ON U.id = S.unidad_medida_id
+		WHERE S.estacion_id = :estacion_id
+		"""
+	return json.dumps([dict(r) for r in conn.execute(stmt, {'estacion_id' : estacion_id})])
