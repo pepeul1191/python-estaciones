09e6bd808db93eb512b276d870ed16d3f3816c45
Pepe Valdivia <jvaldivia@softweb.pe>
2017-12-21 15:47:32 -0500

    CRUD de sensores FDD

--- test/rspec/sensor.rb
+++ test/rspec/sensor.rb
@@ -21,4 +21,162 @@ def listar
   end
 end
 
+def crear
+  RSpec.describe App do
+    describe "2. Crear sensores: " do
+      it '2.1 Conexión con backend' do
+        url = 'test/conexion'
+        test = App.new(url)
+        test.get()
+        expect(test.response.code).to eq(200)
+      end
+      it '2.2 Crear sensores' do
+        data = {
+          :nuevos => [
+            {
+              :id => 'tablaAsociacion_482',
+              :nombre => 'Corbett',  
+              :descripcion => '1 - Tincidunt tempus. Donec vitae sapien ut libero', 
+              :unidad_medida_id => 1,
+              :estacion_id => 2
+            },
+            {
+              :id => 'tablaAsociacion_483',
+              :nombre => 'Channa', 
+              :descripcion => '2 - Tincidunt tempus. Donec vitae sapien ut libero', 
+              :unidad_medida_id => 1,
+              :estacion_id => 2
+            },
+            {
+              :id => 'tablaAsociacion_484',
+              :nombre => 'Maxy',  
+              :descripcion => '3 - Tincidunt tempus. Donec vitae sapien ut libero', 
+              :unidad_medida_id => 1,
+              :estacion_id => 2
+            },
+            {
+              :id => 'tablaAsociacion_485',
+              :nombre => 'Alphard', 
+              :descripcion => '4 - Tincidunt tempus. Donec vitae sapien ut libero', 
+              :unidad_medida_id => 1,
+              :estacion_id => 2
+            }  
+          ],
+          :editados => [],  
+          :eliminados => [],
+          :extra => {
+            :estacion_id => 2
+          }
+        }.to_json
+        url = 'sensor/guardar?data=' + data
+        test = App.new(url)
+        test.post()
+        expect(test.response.code).to eq(200)
+        expect(test.response.body).not_to include('error')
+        expect(test.response.body).to include('Se ha registrado los cambios en los sensores')
+        expect(test.response.body).to include('nuevo_id')
+        expect(test.response.body).to include('success')
+      end
+    end
+  end
+end
+
+def editar
+  RSpec.describe App do
+    describe "3. Editar sensores: " do
+      it '3.1 Conexión con backend' do
+        url = 'test/conexion'
+        test = App.new(url)
+        test.get()
+        expect(test.response.code).to eq(200)
+      end
+      it '3.2 Editar sensores' do
+        data = {
+          :nuevos => [
+          ],
+          :editados => [
+            {
+              :id => '21',
+              :nombre => 'Corbett',  
+              :descripcion => '1x - Tincidunt tempus. Donec vitae sapien ut libero', 
+              :unidad_medida_id => 1,
+              :estacion_id => 2
+            },
+            {
+              :id => '22',
+              :nombre => 'Channa', 
+              :descripcion => '2x - Tincidunt tempus. Donec vitae sapien ut libero', 
+              :unidad_medida_id => 1,
+              :estacion_id => 2
+            },
+            {
+              :id => '23',
+              :nombre => 'Maxy',  
+              :descripcion => '3x - Tincidunt tempus. Donec vitae sapien ut libero', 
+              :unidad_medida_id => 1,
+              :estacion_id => 2
+            },
+            {
+              :id => '24',
+              :nombre => 'Alphard', 
+              :descripcion => '4x - Tincidunt tempus. Donec vitae sapien ut libero', 
+              :unidad_medida_id => 1,
+              :estacion_id => 2
+            }  
+          ],  
+          :eliminados => [],
+          :extra => {
+            :estacion_id => 2
+          }
+        }.to_json
+        url = 'sensor/guardar?data=' + data
+        test = App.new(url)
+        test.post()
+        expect(test.response.code).to eq(200)
+        expect(test.response.body).not_to include('error')
+        expect(test.response.body).to include('Se ha registrado los cambios en los sensores')
+        expect(test.response.body).to include('success')
+      end
+    end
+  end
+end
+
+def eliminar
+  RSpec.describe App do
+    describe "4. Eliminar sensores: " do
+      it '4.1 Conexión con backend' do
+        url = 'test/conexion'
+        test = App.new(url)
+        test.get()
+        expect(test.response.code).to eq(200)
+      end
+      it '4.2 Eliminar sensores' do
+        data = {
+          :nuevos => [
+          ],
+          :editados => [
+          ],  
+          :eliminados => [
+            21,22,23,24
+          ],
+          :extra => {
+            :estacion_id => 2
+          }
+        }.to_json
+        url = 'sensor/guardar?data=' + data
+        test = App.new(url)
+        test.post()
+        expect(test.response.code).to eq(200)
+        expect(test.response.body).not_to include('error')
+        expect(test.response.body).to include('Se ha registrado los cambios en los sensores')
+        expect(test.response.body).to include('success')
+      end
+    end
+  end
+end
+
+
 listar
+#crear
+#editar
+eliminar
--- views/sensor.py
+++ views/sensor.py
@@ -23,3 +23,38 @@ def listar_estacion(estacion_id):
 		WHERE S.estacion_id = :estacion_id
 		"""
 	return json.dumps([dict(r) for r in conn.execute(stmt, {'estacion_id' : estacion_id})])
+
+@sensor_view.route('/guardar', method='POST')
+def guardar():
+	data = json.loads(request.query.data)
+	nuevos = data['nuevos']
+	editados = data['editados']
+	eliminados = data['eliminados']
+	estacion_id = data['extra']['estacion_id']
+	array_nuevos = []
+	rpta = None
+	session = session_db()
+	try:
+		if len(nuevos) != 0:
+			for nuevo in nuevos:
+				temp_id = nuevo['id']
+				nombre = nuevo['nombre']
+				descripcion = nuevo['descripcion']
+				unidad_medida_id = nuevo['unidad_medida_id']
+				s = Sensor(nombre = nombre, descripcion = descripcion, unidad_medida_id = unidad_medida_id, estacion_id = estacion_id)
+				session.add(s)
+				session.flush()
+				temp = {'temporal' : temp_id, 'nuevo_id' : s.id}
+				array_nuevos.append(temp)
+		if len(editados) != 0:
+			for editado in editados:
+				session.query(Sensor).filter_by(id = editado['id']).update(editado)
+		if len(eliminados) != 0:
+			for id in eliminados:
+				session.query(Sensor).filter_by(id = id).delete()
+		session.commit()
+		rpta = {'tipo_mensaje' : 'success', 'mensaje' : ['Se ha registrado los cambios en los sensores', array_nuevos]}
+	except Exception as e:
+		session.rollback()
+		rpta = {'tipo_mensaje' : 'error', 'mensaje' : ['Se ha producido un error en guardar los sensores', str(e)]}
+	return json.dumps(rpta)
